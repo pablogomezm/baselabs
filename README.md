83d8b5e74130a662ba4e22ebe3650eb711ee7dfd
pablogomezm <pablog.acc@gmail.com>
2025-10-17 17:19:53 -0300

    add a 60 second throttler

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 To run the project:
 ```
-docker-compose up
+docker-compose up --build
 ```
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, seconds, ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: seconds(60),
+          limit: 1,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
