3548b2ca84e12fec9b55226237000b78025d05c6
pablogomezm <pablog.acc@gmail.com>
2025-10-17 18:30:33 -0300

    pnpm add @tanstack/react-query

---
+++ frontend/src/components/BuyCornButton.tsx
@@ -0,0 +1,15 @@
+export function BuyCornButton() {
+  return (
+    <button
+      className='
+        bg-yellow-600 hover:bg-yellow-500
+        text-white font-bold
+        py-3 px-6
+        rounded-lg
+        transition-colors
+      '
+    >
+      Buy Corn
+    </button>
+  );
+}
