03c1ace8eb72fa9187f5db2c43f128bb6b572124
pablogomezm <pablog.acc@gmail.com>
2025-11-09 20:32:05 -0300

    configure prisma client import

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       await this.$connect();
     } catch (error) {
       console.error('Failed to connect to database:', error);
