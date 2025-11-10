9aed0387276a6b37e37b8434e8a66e0b726e0659
pablogomezm <pablog.acc@gmail.com>
2025-11-10 16:20:33 -0300

    remove old app.controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,15 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { ThrottlerModule, seconds, ThrottlerGuard } from '@nestjs/throttler';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      throttlers: [
-        {
-          ttl: seconds(60),
-          limit: 1,
-        },
-      ],
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
