42daaf0cf10b65c7dc07b05f2223a8d065e96209
pablogomezm <pablog.acc@gmail.com>
2025-11-17 15:49:58 -0300

    remove named volumes for node_modules

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "pnpm exec prisma generate"
 pnpm exec prisma generate
+echo "pnpm exec prisma migrate deploy"
 pnpm exec prisma migrate deploy
+echo "pnpm run start:dev"
 pnpm run start:dev
