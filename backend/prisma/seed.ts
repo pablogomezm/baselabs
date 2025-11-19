1d42c90f3f6afedd7071be5bf3defe06938cda65
pablogomezm <pablog.acc@gmail.com>
2025-11-19 16:03:55 -0300

    seed an admin user and a normal user

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient, Role } from '../generated/prisma/client';
+import * as argon2 from 'argon2';
 
 const prisma = new PrismaClient();
 
@@ -12,8 +13,33 @@ async function main() {
       stock: 2,
     },
   });
-
   console.log('Created product:', corn);
+
+  const adminPassword = await argon2.hash('3265plok');
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@gmail.com' },
+    update: {},
+    create: {
+      email: 'admin@gmail.com',
+      name: 'Admin User',
+      password: adminPassword,
+      role: Role.ADMIN,
+    },
+  });
+  console.log('Created admin user:', { ...admin, password: '******' });
+
+  const userPassword = await argon2.hash('3265plok');
+  const user = await prisma.user.upsert({
+    where: { email: 'user1@gmail.com' },
+    update: {},
+    create: {
+      email: 'user1@gmail.com',
+      name: 'User1',
+      password: userPassword,
+      role: Role.USER,
+    },
+  });
+  console.log('Created regular user:', { ...user, password: '******' });
 }
 
 main()
