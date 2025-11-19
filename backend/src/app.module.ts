3a69413c576250c6cc56917a4aaa914bbf41e3de
pablogomezm <pablog.acc@gmail.com>
2025-11-19 14:48:19 -0300

    create products module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { OrdersModule } from './orders/orders.module';
     UsersModule,
     AuthModule,
     OrdersModule,
+    ProductsModule,
   ],
   controllers: [],
   providers: [],
